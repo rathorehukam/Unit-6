dc32daab7d5411a22f268cf2ff6e87f9b3ddc470
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-11-07 12:06:33 +0530

    Create Jessica is overseeing a supermarket and needs to create a custom hash function to store and organize the names of fruits in her inventory. She has already implemented a hash function and wants to validate its correctness using different fruit names.cpp

---
+++ Unit 6/L - 39/Jessica is overseeing a supermarket and needs to create a custom hash function to store and organize the names of fruits in her inventory. She has already implemented a hash function and wants to validate its correctness using different fruit names.cpp
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int HASH_TABLE_SIZE = 10000;
+
+unsigned int customHash(string& key) {
+    unsigned int hashValue = 0;
+
+    for (char ch : key) {
+        hashValue = (hashValue * 29) + ch;
+    }
+
+    return hashValue % HASH_TABLE_SIZE;
+}
+
+int main() {
+    string key;
+    while (true) {
+        cin >> key;
+
+        if (key == "exit") {
+            break;
+        }
+
+        unsigned int hashValue = customHash(key);
+        cout << hashValue << endl;
+    }
+
+    return 0;
+}
