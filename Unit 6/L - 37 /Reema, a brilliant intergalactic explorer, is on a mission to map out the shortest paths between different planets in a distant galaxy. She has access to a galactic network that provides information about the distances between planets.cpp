1b70133bf3aaf9209821bff37779e79182740f7f
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-11-07 11:55:53 +0530

    Create Reema, a brilliant intergalactic explorer, is on a mission to map out the shortest paths between different planets in a distant galaxy. She has access to a galactic network that provides information about the distances between planets.cpp

---
+++ Unit 6/L - 37 /Reema, a brilliant intergalactic explorer, is on a mission to map out the shortest paths between different planets in a distant galaxy. She has access to a galactic network that provides information about the distances between planets.cpp
@@ -0,0 +1,82 @@
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+#define INF 1e7
+#define MAXN 100
+
+int dis[MAXN][MAXN];
+int Next[MAXN][MAXN];
+
+void initialise(int V, int graph[MAXN][MAXN]) {
+    for (int i = 0; i < V; ++i) {
+        for (int j = 0; j < V; ++j) {
+            dis[i][j] = graph[i][j];
+            if (graph[i][j] == INF)
+                Next[i][j] = -1;
+            else
+                Next[i][j] = j;
+        }
+    }
+}
+
+void floydWarshall(int V) {
+    for (int k = 0; k < V; ++k) {
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < V; ++j) {
+                if (dis[i][k] == INF || dis[k][j] == INF)
+                    continue;
+
+                if (dis[i][j] > dis[i][k] + dis[k][j]) {
+                    dis[i][j] = dis[i][k] + dis[k][j];
+                    Next[i][j] = Next[i][k];
+                }
+            }
+        }
+    }
+}
+
+void printPath(int path[], int n) {
+    for (int i = 0; i < n - 1; ++i)
+        cout << path[i] << " -> ";
+    cout << path[n - 1];
+}
+
+int main() {
+    int V;
+    cin >> V;
+
+    int graph[MAXN][MAXN];
+    for (int i = 0; i < V; ++i) {
+        for (int j = 0; j < V; ++j) {
+            cin >> graph[i][j];
+        }
+    }
+
+    initialise(V, graph);
+    floydWarshall(V);
+
+    int source = 0; 
+
+    int dest;
+    cin >> dest;
+
+    int path[MAXN];
+    path[0] = source;
+    int index = 1;
+    while (source != dest) {
+        source = Next[source][dest];
+        path[index++] = source;
+    }
+
+    cout << "Shortest path from Earth to planet " << dest << ": ";
+    printPath(path, index);
+
+    return 0;
+}
