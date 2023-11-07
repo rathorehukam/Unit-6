df26af0ee342bf05809dae6bef96f33ba06ba100
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-11-07 09:19:30 +0530

    Create Hannah is given the task of analyzing graphs and determining the reachability of nodes within the graph.cpp

---
+++ Unit 6/L - 36/Hannah is given the task of analyzing graphs and determining the reachability of nodes within the graph.cpp
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+bool isReachable(int **graph, int u, int v, int n) {
+    return graph[u][v] == 1;
+}
+
+void computeReachability(int **graph, int n) {
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                graph[i][j] = graph[i][j] || (graph[i][k] && graph[k][j]);
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int **graph = new int *[n];
+    for (int i = 0; i < n; ++i) {
+        graph[i] = new int[n];
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> graph[i][j];
+        }
+    }
+
+    computeReachability(graph, n);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+    int u, v;
+    cin >> u >> v;
+
+    if (isReachable(graph, u, v, n)) {
+        cout << "Path Exists";
+    } else {
+        cout << "Path does not Exist";
+    }
+
+    for (int i = 0; i < n; ++i) {
+        delete[] graph[i];
+    }
+    delete[] graph;
+
+    return 0;
+}
