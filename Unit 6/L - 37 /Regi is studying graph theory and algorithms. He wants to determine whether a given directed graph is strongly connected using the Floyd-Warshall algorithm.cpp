45eb96e75075b497a4b34c06a8e3434bfdb5d034
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-11-07 11:58:46 +0530

    Create Regi is studying graph theory and algorithms. He wants to determine whether a given directed graph is strongly connected using the Floyd-Warshall algorithm.cpp

---
+++ Unit 6/L - 37 /Regi is studying graph theory and algorithms. He wants to determine whether a given directed graph is strongly connected using the Floyd-Warshall algorithm.cpp
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+void floydWarshall(int n, int graph[][100]) {
+    int INF = 1e9;
+
+    int reachability[n][100];
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            reachability[i][j] = graph[i][j];
+            if (i != j && graph[i][j] == 0) {
+                reachability[i][j] = INF;
+            }
+        }
+    }
+
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (reachability[i][k] < INF && reachability[k][j] < INF) {
+                    reachability[i][j] = min(reachability[i][j], reachability[i][k] + reachability[k][j]);
+                }
+            }
+        }
+    }
+
+    bool isStronglyConnected = true;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i != j && reachability[i][j] == INF) {
+                isStronglyConnected = false;
+                break;
+            }
+        }
+    }
+
+    if (isStronglyConnected) {
+        cout << "The graph is strongly connected";
+    } else {
+        cout << "The graph is not strongly connected";
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int graph[100][100];
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> graph[i][j];
+        }
+    }
+
+    floydWarshall(n, graph);
+
+    return 0;
+}
